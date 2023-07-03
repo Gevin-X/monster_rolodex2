fb254b3fe29c83c9b054b2a62f04e81db33ea8b3
gevin-x <gemba.gp@gmail.com>
2023-07-04 00:34:36 +0530

    adding file 4 gh pages

---
+++ src/components/search-box/search-box.components.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import './search-box.styles.css'
+
+export const Searchbox = ({placeholder,handleChange})=>(
+    <input
+        className="search"
+        type='search'
+        placeholder={placeholder}
+        onChange = {handleChange} //{e=>{this.setState({searchField:e.target.value} //,()=>console.log(this.state.searchField)   setState can have second argument as a function < setstate is async>  
+         
+    />
+);
